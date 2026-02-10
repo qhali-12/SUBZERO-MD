dd6963c9ee5ddf2a89418a2e0a162b04f0de5bf1
Qhalitech <qhalitech012@gmail.com>
2026-02-10 10:08:34 +0300

    Update settings.js for QhaliTech256

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~qdcM4v",
+  OWNER_NUMBER: "+256769842372", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
